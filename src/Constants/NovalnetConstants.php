e1a4b1977852ac320f7378df67e7a79ad44433ea
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-21 16:28:10 +0530

    Update NovalnetConstants.php

--- src/Constants/NovalnetConstants.php
+++ src/Constants/NovalnetConstants.php
@@ -19,10 +19,11 @@ class NovalnetConstants
     const PAYMENT_URL           = 'https://payport.novalnet.de/v2/payment';
     const PAYMENT_AUTHORIZE_URL = 'https://payport.novalnet.de/v2/authorize';
     const TXN_RESPONSE_URL      = 'https://payport.novalnet.de/v2/transaction_details';
-    const PAYGATE_URL           = 'https://payport.novalnet.de/v2/seamless/payment';
+    const SEAMLESS_PAYMENT_URL  = 'https://payport.novalnet.de/v2/seamless/payment';
     const PAYMENT_CAPTURE_URL   = 'https://payport.novalnet.de/v2/transaction/capture';
     const PAYMENT_VOID_URL      = 'https://payport.novalnet.de/v2/transaction/cancel';
     const PAYMENT_REFUND_URL    = 'https://payport.novalnet.de/v2/transaction/refund';
     const TXN_UPDATE            = 'https://payport.novalnet.de/v2/transaction/update';
     const MERCHANT_DETAILS      = 'https://payport.novalnet.de/v2/merchant/details';
+    const SEAMLESS_PAYMENT_AUTHORIZE_URL  = 'https://payport.novalnet.de/v2/seamless/authorize';
 }
