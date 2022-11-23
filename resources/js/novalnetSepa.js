418906d1976a52ceb6105af044e25c5bf2d0e02c
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-23 14:18:00 +0530

    Update novalnetSepa.js

--- resources/js/novalnetSepa.js
+++ resources/js/novalnetSepa.js
@@ -4,18 +4,19 @@ jQuery(document).ready( function() {
         let iban = jQuery(this).val().replace( /[^a-zA-Z0-9]+/g, "" ).replace( /\s+/g, "" );
         jQuery(this).val(iban);
     });
-    // Validate the BIC when if it is mandatory
-       var ibanCountry = jQuery('#nn_sepa_iban').val().substring(0,2);
-       if (ibanCountry.match(/(?:CH|MC|SM|GB)$/)) {
-         jQuery('#nn_sepa_bic').prop('required', true);
-       } else {
-           jQuery('#nn_sepa_bic').prop('required', false);
-       } 
+   
     // After the form submission disable the action
     jQuery('#novalnet_form').on('submit',function() {
        jQuery('#novalnet_form_btn').prop('disabled', true);
        if(jQuery('#nn_show_birthday').val() == true && (jQuery("#nn_guarantee_year").val() == '' || jQuery("#nn_guarantee_date").val() == '' || jQuery("#nn_guarantee_month").val() == '0')) {
            jQuery('#novalnet_form_btn').prop('disabled', false);
+       }
+        // Validate the BIC when if it is mandatory
+       var ibanCountry = jQuery('#nn_sepa_iban').val().substring(0,2);
+       if (ibanCountry.match(/(?:CH|MC|SM|GB)$/) && jQuery('#nn_sepa_bic').val() == '') {
+            alert('Please enter the BIC');
+            jQuery('#novalnet_form_btn').prop('disabled', false);
+            return false;
        }
        
     });
