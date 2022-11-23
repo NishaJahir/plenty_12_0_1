1dcb41c9eeac29e7bd01fab1d83bfc33f816439a
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-23 12:42:55 +0530

    Update novalnetSepa.js

--- resources/js/novalnetSepa.js
+++ resources/js/novalnetSepa.js
@@ -4,15 +4,15 @@ jQuery(document).ready( function() {
         let iban = jQuery(this).val().replace( /[^a-zA-Z0-9]+/g, "" ).replace( /\s+/g, "" );
         jQuery(this).val(iban);
     });
-    // After the form submission disable the action
-    jQuery('#novalnet_form').on('submit',function() {
-       // Validate the BIC when if it is mandatory
+     // Validate the BIC when if it is mandatory
        var bic = jQuery('#nn_sepa_bic').val().substring(0,2);
        if (bic.match(/(?:CH|MC|SM|GB)$/)) {
          jQuery('#nn_sepa_bic').prop('required', true);
        } else {
            jQuery('#nn_sepa_bic').prop('required', false);
        }
+    // After the form submission disable the action
+    jQuery('#novalnet_form').on('submit',function() {
        jQuery('#novalnet_form_btn').prop('disabled', true);
        if(jQuery('#nn_show_birthday').val() == true && (jQuery("#nn_guarantee_year").val() == '' || jQuery("#nn_guarantee_date").val() == '' || jQuery("#nn_guarantee_month").val() == '0')) {
            jQuery('#novalnet_form_btn').prop('disabled', false);
