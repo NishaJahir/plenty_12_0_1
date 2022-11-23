5172cd6d58b0071fec7d094c16775adab34a6d80
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-23 12:45:31 +0530

    Update novalnetGooglePay.js

--- resources/js/novalnetGooglePay.js
+++ resources/js/novalnetGooglePay.js
@@ -45,6 +45,7 @@ jQuery(document).ready(function() {
                             jQuery('#nn_google_pay_token').val(response.transaction.token);
                              jQuery('#nn_google_pay_do_redirect').val(response.transaction.doRedirect);                               
                             jQuery('#nn_google_pay_form').submit();
+			     jQuery('#nn_google_pay').find('button').prop('disabled', true);
                         } else {
                             // Upon failure, displaying the error text
                             if(response.result.status_text) {
