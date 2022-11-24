328053f1321a3d08b927f49aaf08eefeff3134e8
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-24 19:55:32 +0530

    Update NovalnetGooglePayButtonDataProvider.php

--- src/Providers/DataProvider/NovalnetGooglePayButtonDataProvider.php
+++ src/Providers/DataProvider/NovalnetGooglePayButtonDataProvider.php
@@ -75,7 +75,7 @@ class NovalnetGooglePayButtonDataProvider
                             'clientKey'     => trim($settingsService->getPaymentSettingsValue('novalnet_client_key')),
                             'merchantId'    => $settingsService->getPaymentSettingsValue('payment_active', 'novalnet_googlepay'),
                             'sellerName'    => !empty($sellerName) ? $sellerName : $webstoreHelper->getCurrentWebstoreConfiguration()->name,
-                            'enforce'       => (bool) ($settingsService->getPaymentSettingsValue('enforce', 'novalnet_googlepay') == true),
+                            'enforce'       => ($settingsService->getPaymentSettingsValue('enforce', 'novalnet_googlepay') == 1) ? true : false,
                             'buttonType'    => $settingsService->getPaymentSettingsValue('button_type', 'novalnet_googlepay'),
                             'buttonTheme'   => $settingsService->getPaymentSettingsValue('button_theme', 'novalnet_googlepay'),
                             'buttonHeight'  => $settingsService->getPaymentSettingsValue('button_height', 'novalnet_googlepay'),
