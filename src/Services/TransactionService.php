f0c9a159de0a35ca056353f94efcff0094fcec98
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-11-28 13:18:32 +0530

    Update TransactionService.php

--- src/Services/TransactionService.php
+++ src/Services/TransactionService.php
@@ -43,6 +43,7 @@ class TransactionService
             $transaction->tid                 = $transactionData['tid'];
             $transaction->paymentName         = $transactionData['payment_name'];
             $transaction->additionalInfo      = !empty($transactionData['additional_info']) ? $transactionData['additional_info'] : '0';
+            $this->getLogger(__METHOD__)->error('table insert', $transaction);
             $database->save($transaction);
         } catch (\Exception $e) {
             $this->getLogger(__METHOD__)->error('Callback table insert failed!.', $e);
